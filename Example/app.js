001694a15eecfcd077ae25ea3c8bc1be18859df7
npomfret <nick-github@snowmonkey.co.uk>
2016-11-08 16:32:06 +0000

    adding example app

---
+++ Example/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {AppRegistry, TouchableOpacity, StyleSheet, Text, View} from 'react-native';
+import RNFS from 'react-native-fs';
+import RNCloudFs from 'react-native-cloud-fs';
+
+export default class RNCloudFSExample extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log("RNCloudFs",RNCloudFs);
+    this.saveFile = this.saveFile.bind(this);
+  }
+
+  saveFile() {
+    const tmpFilePath = RNFS.DocumentDirectoryPath + '/test.txt';
+
+    RNFS.writeFile(tmpFilePath, 'This is a test file ' + new Date().toISOString(), 'utf8')
+      .then(() => {
+        return RNCloudFs.copyToCloud(tmpFilePath, "folder-a/folder-b/my-file.txt", null);
+      })
+      .catch((err) => {
+        console.warn("failed", err);
+      })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this.saveFile}><Text>press to save a file to icloud</Text></TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+AppRegistry.registerComponent('RNCloudFSExample', () => RNCloudFSExample);
+
---
+++ Example/index.android.js
@@ -0,0 +1,2 @@
+import Example from './app';
+
