5792712bd33786f579f6ed534e9c4968ecf3b7b8
npomfret <nick-github@snowmonkey.co.uk>
2016-11-09 12:23:20 +0000

    fix ios implemention so that it doesn't move the file but copies it instead

--- Example/app.js
+++ Example/app.js
@@ -1,34 +1,52 @@
 'use strict';
 
-import React, { Component } from 'react';
-import {AppRegistry, TouchableOpacity, StyleSheet, Text, View} from 'react-native';
-import RNFS from 'react-native-fs';
-import RNCloudFs from 'react-native-cloud-fs';
+import React, {Component} from "react";
+import {AppRegistry, TouchableOpacity, StyleSheet, Text, View, TextInput} from "react-native";
+import RNFS from "react-native-fs";
+import RNCloudFs from "react-native-cloud-fs";
 
 export default class RNCloudFSExample extends Component {
   constructor(props) {
     super(props);
 
-    console.log("RNCloudFs",RNCloudFs);
     this.saveFile = this.saveFile.bind(this);
+
+    this.state = {
+      tmpFilePath: "fff"
+    }
   }
 
-  saveFile() {
+  componentDidMount() {
     const tmpFilePath = RNFS.DocumentDirectoryPath + '/test.txt';
 
     RNFS.writeFile(tmpFilePath, 'This is a test file ' + new Date().toISOString(), 'utf8')
       .then(() => {
-        return RNCloudFs.copyToCloud(tmpFilePath, "folder-a/folder-b/my-file.txt", null);
+        this.setState({tmpFilePath: tmpFilePath})
+      })
+  }
+
+  saveFile() {
+    return RNCloudFs.copyToCloud(this.state.tmpFilePath, "folder-a/folder-b/my-file." + new Date().toISOString() + ".txt", null)
+      .then((res) => {
+        console.log("it worked", res);
       })
       .catch((err) => {
-        console.warn("failed", err);
+        console.warn("it failed", err);
       })
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={this.saveFile}><Text>press to save a file to icloud</Text></TouchableOpacity>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', padding: 8}}>
+        <View style={styles.container}>
+          <View style={{flex: 1}}>
+            <Text style={styles.heading}>absolute path</Text>
+            <TextInput style={{height: 20, borderColor: 'gray', borderWidth: 1, fontSize: 10}} value={this.state.tmpFilePath}/>
+            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+              <TouchableOpacity onPress={this.saveFile}><Text style={styles.button}>save to cloud</Text></TouchableOpacity>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
@@ -36,10 +54,27 @@ export default class RNCloudFSExample extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 4,
+    marginBottom: 8,
+    flexDirection: 'row',
+    padding: 4
+  },
+  heading: {
+    fontSize: 12,
+    textAlign: 'left'
+  },
+  button: {
+    margin: 2,
+    padding: 2,
+    borderWidth: 1,
+    fontSize: 10,
+    borderRadius: 4,
+    overflow: 'hidden',
+    backgroundColor: 'black',
+    color: 'white',
+    fontWeight: '600'
   }
 });
 
--- ios/RNCloudFs.m
+++ ios/RNCloudFs.m
@@ -15,45 +15,52 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(copyToCloud:(NSString *)sourceUri :(NSString *)targetRelativePath :(NSString *)mimeType
                   resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-{
-    // mimeType is ignored in iOS
+                  rejecter:(RCTPromiseRejectBlock)reject) {
 
-    NSFileManager* fileManager = [NSFileManager defaultManager];
+    // mimeType is ignored for iOS
 
-    if (![fileManager fileExistsAtPath:sourceUri isDirectory:nil]) {
-        NSLog(@"source file does not exist %@", sourceUri);
-        return reject(@"ENOENT", [NSString stringWithFormat:@"ENOENT: no such file or directory, open '%@'", sourceUri], nil);
-    }
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSFileManager* fileManager = [NSFileManager defaultManager];
 
-    NSURL *sourceURL = [NSURL fileURLWithPath:sourceUri];
-
-    [self rootDirectoryForICloud:^(NSURL *ubiquityURL) {
-        if (ubiquityURL) {
-            NSURL* targetFile = [ubiquityURL URLByAppendingPathComponent:targetRelativePath];
-            NSLog(@"Target file: %@", targetFile.path);
-
-            NSURL *dir = [targetFile URLByDeletingLastPathComponent];
-            NSLog(@"Target dir: %@", dir.path);
-            if (![fileManager fileExistsAtPath:dir.path isDirectory:nil]) {
-                [fileManager createDirectoryAtURL:dir withIntermediateDirectories:YES attributes:nil error:nil];
-            }
-
-            NSError *error;
-            if ([fileManager setUbiquitous:YES itemAtURL:sourceURL destinationURL:targetFile error:&error]) {
-                return resolve(@{@"path": targetFile.absoluteString});
-            } else {
-                NSLog(@"Error occurred: %@", error);
-                NSString *codeWithDomain = [NSString stringWithFormat:@"E%@%zd", error.domain.uppercaseString, error.code];
-                return reject(codeWithDomain, error.localizedDescription, error);
-            }
-        } else {
-            NSLog(@"Could not retrieve a ubiquityURL");
-            return reject(@"ENOENT", [NSString stringWithFormat:@"ENOENT: could not copy to iCloud drive '%@'", sourceUri.absolutePath], nil);
+        if (![fileManager fileExistsAtPath:sourceUri isDirectory:nil]) {
+            NSLog(@"source file does not exist %@", sourceUri);
+            return reject(@"ENOENT", [NSString stringWithFormat:@"ENOENT: no such file or directory, open '%@'", sourceUri], nil);
         }
-    }];
-}
 
+        NSURL *sourceURL = [NSURL fileURLWithPath:sourceUri];
+
+        [self rootDirectoryForICloud:^(NSURL *ubiquityURL) {
+            if (ubiquityURL) {
+                NSURL* targetFile = [ubiquityURL URLByAppendingPathComponent:targetRelativePath];
+                NSLog(@"Target file: %@", targetFile.path);
+
+                NSURL *dir = [targetFile URLByDeletingLastPathComponent];
+                if (![fileManager fileExistsAtPath:dir.path isDirectory:nil]) {
+                    [fileManager createDirectoryAtURL:dir withIntermediateDirectories:YES attributes:nil error:nil];
+                }
+
+                // todo: figure out how to *copy* to icloud drive
+                // ...setUbiquitous will move the file instead of copying it, so as a work around lets copy it to a tmp file first
+                NSString *filename = [sourceUri lastPathComponent];
+                NSString *tempFile = [NSTemporaryDirectory() stringByAppendingPathComponent:filename];
+                NSError *error;
+                [fileManager copyItemAtPath:sourceURL toPath:tempFile error:&error];
+                NSLog(@"Moving file %@ to %@", tempFile, targetFile);
+
+                if ([fileManager setUbiquitous:YES itemAtURL:[NSURL fileURLWithPath:tempFile] destinationURL:targetFile error:&error]) {
+                    return resolve(@{@"path": targetFile.absoluteString});
+                } else {
+                    NSLog(@"Error occurred: %@", error);
+                    NSString *codeWithDomain = [NSString stringWithFormat:@"E%@%zd", error.domain.uppercaseString, error.code];
+                    return reject(codeWithDomain, error.localizedDescription, error);
+                }
+            } else {
+                NSLog(@"Could not retrieve a ubiquityURL");
+                return reject(@"ENOENT", [NSString stringWithFormat:@"ENOENT: could not copy to iCloud drive '%@'", sourceUri.absolutePath], nil);
+            }
+        }];
+    });
+}
 
 - (void)rootDirectoryForICloud:(void (^)(NSURL *))completionHandler {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
